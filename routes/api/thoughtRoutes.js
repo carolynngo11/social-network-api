5252990fcd68fef68ac6d47e8568bab03a843dba
Carolyn Ngo <carolynngo11@yahoo.com>
2022-05-16 22:05:10 -0500

    added to routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,23 @@
+const router= require('express').Router();
+const {
+    getThoughts,
+    getThought,
+    addThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController')
+
+router.route('/').get(getThoughts)
+
+router.route("/").get(getThoughts).post(addThought);
+
+router.route("/:thoughtId").get(getThought).put(updateThought).delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+
+module.exports = router;
