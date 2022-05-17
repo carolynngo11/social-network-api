bf4f7d39c3ab68423256ed1d9f648015cda79255
Carolyn Ngo <carolynngo11@yahoo.com>
2022-05-16 20:12:16 -0500

    created data, models, route

---
+++ utils/data.js
@@ -0,0 +1,33 @@
+const names = [
+    'Aaron',
+    'Smith',
+    'Zenith',
+    'Brook',
+    'Zion'
+];
+
+const reactionDescriptions = [
+    'Great',
+    'Nice',
+    'Bad',
+    'Good'
+];
+
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const getRandomName = () =>
+  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+
+// Function to generate random assignments that we can add to student object.
+const getRandomReaction = (int) => {
+  const results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      reactionName: getRandomArrItem(reactionDescriptions)
+    });
+  }
+  return results;
+};
+
+// Export the functions for use in seed.js
+module.exports = { getRandomName, getRandomReaction };
