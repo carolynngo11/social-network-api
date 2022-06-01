69e0f90f3ffe28dd99feb7d6929ff52efb7f76de
Carolyn Ngo <carolynngo11@yahoo.com>
2022-05-31 23:33:30 -0500

    added to controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,4 @@
-const { Thought } = require("../models");
+const { User, Thought } = require("../models");
 
 module.exports = {
     // Get all thoughts
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { User } = require("../models");
+const { User, Thought } = require("../models");
 
 module.exports = {
     // Get all users
@@ -49,28 +49,24 @@ module.exports = {
       .then((user) =>
       !user
         ? res.status(404).json({ message: 'No user with this ID.'})
-        : res.json(user)
+        : Thought.deleteMany({ username: user.username })
       )
       .catch((err) => res.status(500).json(err));
   },
     // Add Friend
-    addFriend(req, res) {  
-        // console.log('You are adding a friend');
-        // console.log(req.body);
-        User.findOneAndUpdate(
-            { _id: req.params.userId },
-            { $addToSet: { friend: req.body } },
-            { runValidators: true, new: true }
-          )
-            .then((user) =>
-              !user
-                ? res
-                    .status(404)
-                    .json({ message: 'No user found with this ID!' })
-                : res.json(user)
-            )
-            .catch((err) => res.status(500).json(err));
-        },
+    addFriend(req, res) {
+      User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { runValidators: true, new: true }
+      )
+        .then((user) =>
+          !user
+            ? res.status(404).json({ message: 'No user found with that ID.' })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
 
     // Delete Friend
     removeFriend(req, res) {
